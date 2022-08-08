833dd6b363c3b0f317ddc2d87360ce90ba5c677e
Adamjatim <adamrahmatilahi@gmail.com>
2022-08-09 04:20:08 +0700

    Update about UI

--- scripts/blurFilterEffect.js
+++ scripts/blurFilterEffect.js
@@ -8,6 +8,8 @@ window.addEventListener('DOMContentLoaded', () => {
         minWidth: 200.00,
         scale: 1.00,    
         scaleMobile: 1.00,
+        color: 0xff3f81,
+        backgroundColor: 0x23153c,
         maxDistance: 20.00,
         spacing: 20.00
     })
